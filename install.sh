36997fe45a53631e83f787f218af64b76b1df818
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2024-12-26 17:04:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,7 +80,6 @@ chown -R "$username":"$username" gimp-dots
 rm -rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
 rm -rf /home/"$username"/.config/GIMP/*
 cd gimp-dots/Gimp || exit
-cp -R 3.0 /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/
 cp -R 3.0 /home/"$username"/.config/GIMP/
 cd "$builddir" || exit
 
