4d86ad06c6a7ab272cfe2486e4650b3efbff628f
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-02-20 11:15:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -74,12 +74,15 @@ wait
 
 # Gimp dotfiles
 git clone https://github.com/Piercingxx/gimp-dots.git
-chmod u+x gimp-dots
+chmod -R u+x gimp-dots
 chown -R "$username":"$username" gimp-dots
-rm -rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
-rm -rf /home/"$username"/.config/GIMP/*
+rm -Rf /home/"$username"/.var/app/org.gimp.GIMP/config/GIMP/*
+rm -Rf /home/"$username"/.config/GIMP/*
+mkdir /home/"$username"/.config/GIMP/3.0
+chown -R "$username":"$username" /home/"$username"/.config/GIMP
 cd gimp-dots/Gimp || exit
-cp -R 3.0 /home/"$username"/.config/GIMP/
+cp -R "3.0" /home/"$username"/.config/GIMP/
+chown "$username":"$username" -R /home/"$username"/.config/GIMP
 cd "$builddir" || exit
 
 
